fa449e2a8ca8b4117425ac8eb8a09c0cabba0b57
Mateen Afshari <mateen.afshari@gmail.com>
2023-07-01 18:48:21 -0400

    disable ssl

--- louslist/views.py
+++ louslist/views.py
@@ -12,13 +12,16 @@ from django.db.models import Q
 import ssl
 import urllib, json
 import requests
+import urllib3
 from .models import Comment, Course, Schedule, User, Profile, Relationship
+from urllib3.exceptions import InsecureRequestWarning
 
 
 class IndexView(generic.ListView):
     template_name = 'louslist/index.html'
 
     def get_context_data(self, **kwargs):
+        requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
         context = super(IndexView, self).get_context_data(**kwargs)
         url = "https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearchOptions?institution=UVA01&term=1228"
         resp = requests.get(url, verify=False)
@@ -355,7 +358,64 @@ class DepartmentView(generic.ListView):
         response = requests.get(url, verify=False)
         data = response.json()
 
-       
+        # for course in data:
+            
+        #     try:
+                
+        #         Course.objects.get(course_id=course['class_nbr']) 
+
+        #     except:
+        #         form = Course()
+        #         form.title = course["descr"] 
+        #         form.subject = course["subject"]
+        #         form.number = course["catalog_nbr"]
+        #         form.section = course["class_section"]
+        #         if "-" in str(course["units"]):
+        #             form.credits = int(str(course["units"])[0])
+        #         else:
+        #             form.credits = course["units"]
+        #         form.instructor = course["instructors"][0]["name"] 
+                
+        #         form.location = course["meetings"][0]["facility_descr"] 
+        #         form.course_id = course["class_nbr"]
+                
+                
+        #         # Given string
+        #         time_str = course["meetings"][0]["start_time"]
+        #         # Extract the time portion without the timezone offset
+        #         time_str = time_str.split('-')[0]
+        #         if len(time_str) >= 2:
+        #             # Define the format of the time string
+        #             time_format = "%H.%M.%S.%f"
+
+        #             # Convert the string to a datetime object
+        #             time_obj = datetime.datetime.strptime(time_str, time_format)
+
+        #             # Format the datetime object to display in 12-hour format without seconds and microseconds
+        #             time_str_12h = time_obj.strftime("%I:%M %p")
+
+                    
+        #             time_str2 = course["meetings"][0]["end_time"]
+        #             # Extract the time portion without the timezone offset
+        #             time_str2 = time_str2.split('-')[0]
+
+        #             # Define the format of the time string
+        #             time_format2 = "%H.%M.%S.%f"
+
+        #             # Convert the string to a datetime object
+        #             time_obj2 = datetime.datetime.strptime(time_str2, time_format2)
+        #             time_str_12h2 = time_obj2.strftime("%I:%M %p")
+
+        #             # Format the datetime object to display in 12-hour format without seconds and microseconds
+        #             form.time = str(time_str_12h) + " - " + str(time_str_12h2)
+        #         else:
+        #             form.time = "- -"
+
+        #         form.days = course["meetings"][0]["days"]
+                
+
+        #         form.save()
+        #         print("added")
         context= {
             'data' : data,
         }
